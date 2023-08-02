6e61cf3de027a32ce891e612b5c38c450d696b27
arunppsg <arunppsg@gmail.com>
2023-08-02 14:19:03 +0530

    prepare delaney finetune dataset

--- chemberta3/benchmarking/prepare_data.sh
+++ chemberta3/benchmarking/prepare_data.sh
@@ -1,11 +1,14 @@
 # preparing zinc5k datasets
 ## dataset used by chemberta-mlm, chemberta-mtr pretraining models
-python3 benchmark.py --prepare_data --dataset_name zinc5k --featurizer_name dummy 
+# python3 benchmark.py --prepare_data --dataset_name zinc5k --featurizer_name dummy 
 ## dataset used by grover model 
-python3 benchmark.py --prepare_data --dataset_name zinc5k --featurizer_name grover
+# python3 benchmark.py --prepare_data --dataset_name zinc5k --featurizer_name grover
 ## dataset used by infograph model
-python3 benchmark.py --prepare_data --dataset_name zinc5k --featurizer_name molgraph 
+# python3 benchmark.py --prepare_data --dataset_name zinc5k --featurizer_name molgraph 
 ## dataset used by infomax3d model
-python3 benchmark.py --prepare_data --dataset_name zinc5k --featurizer_name rdkit-conformer
+# python3 benchmark.py --prepare_data --dataset_name zinc5k --featurizer_name rdkit-conformer
 ## dataset used by snap model
-python3 benchmark.py --prepare_data --dataset_name zinc5k --featurizer_name snap
+# python3 benchmark.py --prepare_data --dataset_name zinc5k --featurizer_name snap
+#
+# preparing delaney dataset for finetuning
+python3 benchmark.py --prepare_data --dataset_name delaney --featurizer_name dummy
